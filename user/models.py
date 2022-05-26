52cc7170b0e5932983927869f7879a9ca45d9c16
J <joras.custodio@gmail.com>
2022-05-26 08:38:29 -0300

    password is now a textfield, also has a user field

--- user/models.py
+++ user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class User(models.Model):
     Username = models.CharField(max_length=200)
-    Password = models.CharField(max_length=200)
+    Password = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
