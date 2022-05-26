c32d330a0450035dada78e1a3785b2e0f39a8bfd
J <joras.custodio@gmail.com>
2022-05-26 09:04:14 -0300

    added friend requests to models

--- user/models.py
+++ user/models.py
@@ -7,4 +7,12 @@ class User(AbstractUser):
     # content = models.TextField()
     friends = models.ManyToManyField("User", blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Friend_Request(models.Model):
+    from_user = models.ForeignKey(
+        User, related_name='from_ser', on_delete=models.CASCADE
+    )
+    to_user = models.ForeignKey(
+        User, related_name = 'to_user', on_delete=models.CASCADE
+    )
     
