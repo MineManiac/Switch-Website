df16c8fcfbb82af982ade63be84470dac332d96d
Diogo Duarte <diogord@al.insper.edu.br>
2022-05-26 09:12:06 -0300

    Retriving info that was deleted

--- user/views.py
+++ user/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import Http404
+
+import user
 from .models import User
 from .serializers import UserSerializer
 import requests
@@ -23,18 +25,26 @@ def novidade_games(request):
 def index(request):
     return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
 
+
+@api_view(['GET', 'POST'])
+def api_note(request, note_id):
+    try:
+        user = User.objects.get(id=note_id)
+    except User.DoesNotExist:
+        raise Http404()
+
+    if request.method == 'POST':
+        new_note_data = request.data
+        user.title = new_note_data['title']
+        user.content = new_note_data['content']
+        user.save()
+    
+    serialized_user = UserSerializer(user)
+    return Response(serialized_user.data)
+
 @api_view(['GET'])
 def api_news(request):
 
     data = novidade_games()
-
-    
-
-    # if request.method == 'GET':
-    #     Nota = Note(Pais = data['country'], 
-    #     NumCasos = data['cases']['total'], 
-    #     NumNovosCasos = data['cases']['new'],
-    #     NumMortes = data['deaths']['total'],
-    #     NumNovasMortes = data['deaths']['new'])
 
     print(data)
