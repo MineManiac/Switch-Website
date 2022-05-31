6589349eafd755d3b0b564693aedd4f2fa03199b
J <joras.custodio@gmail.com>
2022-05-31 08:10:32 -0300

    whatever

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.novidade_games, name='news'),
     path('api/news', views.api_news),
+    path('api/user', views.api_user),
     path('send_friend_request/<int:userID>/',
         views.send_friend_request, name='send friend request'),
     path('accept_friend_request/<int:userID>/',
--- user/views.py
+++ user/views.py
@@ -31,16 +31,16 @@ def index(request):
 
 
 @api_view(['GET', 'POST'])
-def api_note(request, note_id):
+def api_user(request, user_id):
     try:
-        user = User.objects.get(id=note_id)
+        user = User.objects.get(id = user_id)
     except User.DoesNotExist:
         raise Http404()
 
     if request.method == 'POST':
-        new_note_data = request.data
-        user.title = new_note_data['title']
-        user.content = new_note_data['content']
+        new_user_data = request.data
+        # user.title = new_note_data['title']
+        user.friend = new_user_data['friend']
         user.save()
     
     serialized_user = UserSerializer(user)
@@ -54,9 +54,9 @@ def api_news(request):
     print(data)
 
 @login_required
-def send_friend_request(request, userID):
+def send_friend_request(request, user_id):
     from_user = request.user
-    to_user = User.objects.get(id=userID)
+    to_user = User.objects.get(id = user_id)
     friend_request, created = Friend_Request.objects.get_or_create(
         from_user=from_user, to_user = to_user)
     if created:
@@ -65,8 +65,8 @@ def send_friend_request(request, userID):
         return HttpResponse('friend request was alredy sent')
 
 @login_required
-def accept_friend_request(request, requestID):
-    friend_request = Friend_Request.objects.get(id=requestID)
+def accept_friend_request(request, request_id):
+    friend_request = Friend_Request.objects.get(id=request_id)
     if friend_request.to_user == request.user:
         friend_request.to_user.friends.add(friend_request.from_user)
         friend_request.delete()
