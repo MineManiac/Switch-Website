0d2e6d7d8f2a30c40816f751d16eeed3a94f3a70
Renato Laffranchi Falcão <renatolf1@al.insper.edu.br>
2022-06-14 22:14:55 -0300

    Update settings.py

--- nintendo/settings.py
+++ nintendo/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 #ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['aqueous-sea-40270.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['peaceful-brook-18641.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
